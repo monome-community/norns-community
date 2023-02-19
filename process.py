7ebca023ca120f0050d9c999c16e8c7aab00c759
brian crabtree <tehn@monome.org>
2023-02-18 19:09:43 -0500

    author table

--- process.py
+++ process.py
@@ -9,10 +9,32 @@ print(">> parse catalog")
 cat = json.load(open('community.json','r'))
 print("entries:", len(cat['entries']))
 
+a = {}
+
 print(">> generating index.md")
 f = open('index.md','w')
 for e in cat['entries']:
     f.write('['+e['project_name']+'](/'+e['project_name']+') - ['
             +e['author']+'](/'+e['author']+') - '
             +e['description']+"  \n")
+    # generate authors list
+    a.setdefault(e['author'],[]).append(e)
+    # TODO generate script page here
+f.write('\n---\n\ncontributors\n\n')
+for k,v in sorted(a.items()):
+    # maybe don't include script count
+    f.write(k+" ("+str(len(v))+") ")
 f.close()
+
+#print(json.dumps(a,indent=2))
+
+# scripts are not sorted
+print(">> generating author pages")
+for k,v in sorted(a.items()):
+    #f = open('authors.md','w')
+    print(k,len(v))
+    #for e in v:
+        #f.write('['+e['author']+'](/'+e['author']+') - ['
+            #+e['project_name']+'](/'+e['project_name']+') - '
+            #+e['description']+"  \n")
+    #f.close()

d083b851cd6a2e3ceffbf0c8dbd0385c376aabd4
Tyler Etters <tyler@etters.co>
2023-03-05 20:04:29 -0800

    refactor

--- 02-build.py
+++ 02-build.py
@@ -8,14 +8,7 @@ this build script uses:
  - "projects" when operating user-facing pages.
 
 todo:
- - refactor project:
-   - move authors to project['author']
-   - add description to project['description']
-   - sanitized and display names for project name and author name
- - index page:
-   - sorted (requires project dict refactor)
  - search page
- - community tags -> jekyll tags
  - other things...
 '''
 
@@ -28,160 +21,132 @@ community_path = './community.json'
 pages_path = '_pages'
 authors_yml_path = '_data/authors.yml'
 projects_path = pages_path + '/projects'
-community = {} # the raw community.json, parsed to a dict
-projects = {}  # santized entries dict with author assosciations
-authors = []   # list of all authors
 
-# log a message
+
+
+
+# FUNCTIONS
+# FUNCTIONS
+# FUNCTIONS
+
 def log(msg):
   print('>> ' + msg)
 
-# only allow alphanumeric, dashes, and underscores
 def sanitize(str):
+  # only allow alphanumeric, dashes, and underscores
   return re.sub('[^a-zA-Z0-9\-_]', '', str)
 
 
 
 
-# PARSE
-# PARSE
-# PARSE
+# CLASSES
+# CLASSES
+# CLASSES
+
+class Project():
+
+  def __init__(self, entry):
+    self.raw_name = entry['project_name']
+    self.sanitized_name = sanitize(self.raw_name)
+    self.permalink = '/' + self.sanitized_name
+    self.authors = []
+    self.description = entry['description'] if 'description' in entry else ''
+    self.project_url = entry['project_url'] if 'project_url' in entry else ''
+    self.discussion_url = entry['discussion_url'] if 'discussion_url' in entry else ''
+    self.documentation_url = entry['documentation_url'] if 'documentation_url' in entry else ''
+    self.tags = entry['tags'] if 'tags' in entry else ''
+
+  def associate_author(self, author_raw_name):
+    self.authors.append(sanitize(author_raw_name))
+
+  def get_authors_in_alphabetical_order(self):
+    return sorted(self.authors, key=lambda author: author.casefold())
+
+
+
+
+class Author():
+
+  def __init__(self, raw_name):
+    self.raw_name = raw_name 
+    self.sanitized_name = sanitize(raw_name)
+    self.permalink = '/author#' + self.sanitized_name
+
+
+
+
+class CommunityData():
+
+  def __init__(self, community_path):
+    self.community = json.load(open(community_path, 'r'))
+    self.authors = {}
+    self.projects = {}
+
+  def build(self):
+    for entry in self.community['entries']:
+      # defend against a missing project_name or author
+      if not 'project_name' in entry.keys():
+        log('error. entry is somehow missing a project_name, skipping...')
+        continue
+      elif not 'author' in entry.keys():
+        log('error. entry is somehow missing an author, skipping...')
+        continue
+      else:
+        self.add_project(entry)
+
+  # note: projects and authors have a many-to-many relationship
+  def add_project(self, entry):
+    if not entry['project_name'] in self.projects.keys():
+      log('adding ' + entry['project_name'])
+      project = Project(entry)
+      self.projects[project.raw_name] = project
+      # multiple authors are separated by a space
+      author_raw_name = entry['author']
+      if ' ' in author_raw_name:
+        script_authors = author_raw_name.split()
+        for author_raw_name in script_authors:
+          project.associate_author(author_raw_name)
+          self.add_author(author_raw_name)
+      else:
+        project.associate_author(author_raw_name)
+        self.add_author(author_raw_name)
+
+  # add a new author if they don't already exist
+  def add_author(self, author_raw_name):
+    if not author_raw_name in self.authors.keys():
+      self.authors[author_raw_name] = Author(author_raw_name)
+
+  # return a list of authors in alphabetical order
+  def get_authors_in_alphabetical_order(self):
+    return sorted(self.authors.values(), key=lambda author: author.raw_name.casefold())
+  
+  # return a list of projects in alphabetical order
+  def get_projects_in_alphabetical_order(self):
+    return sorted(self.projects.values(), key=lambda project: project.raw_name.casefold())
+
+  
+
+# SETUP
+# SETUP
+# SETUP
+
 log('attempting to parse catalog at ' + community_path + '...')
 if not os.path.exists(community_path):
   log('error. ' + community_path + ' not found. is there a problem with ./01-curl.sh?')
   log('aborting...')
   exit()
-else:
-  community = json.load(open(community_path, 'r'))
+
+community_data = CommunityData(community_path)
+community_data.build()
 log('done.')
 
 log('copying community.json to ./_data for jekyll...')
 subprocess.Popen('cp ./community.json ./_data/community.json', shell=True)
 log('done.')
 
-
-
-
-
-# PRE-PROCESSING
-# PRE-PROCESSING
-# PRE-PROCESSING
-log('pre-processing authors & projects...')
-for entry in community['entries']:
-  project_name = ''
-  # defend against a missing project_name or author
-  if not 'project_name' in entry.keys():
-    log('error. entry is somehow missing a project_name, skipping...')
-    continue
-  elif not 'author' in entry.keys():
-    log('error. entry is somehow missing an author, skipping...')
-    continue
-  else:
-    project_name = sanitize(entry['project_name'])
-    log('added project: ' + project_name)
-    projects[project_name] = []
-
-  # check for multiple authors
-  if ' ' in entry['author']:
-    # add each author
-    split = entry['author'].split()
-    # sanitize each
-    split = list(map(sanitize, split))
-    authors = authors + split
-    projects[project_name] = split
-  else:
-    # add a lone author
-    authors.append(sanitize(entry['author']))
-    projects[project_name].append(entry['author'])
-log('done.')
-
-log('deduping & sorting authors...')
-authors = list(set(authors))
-authors.sort()
-log(str(len(authors)) + ' authors found.')
-log('done.')
-
-log('sorting projects...')
-projects = dict(sorted(projects.items()))
-log(str(len(projects)) + ' authors found.')
-log('done.')
-
-log('raw output of authors & projects for debugging:')
-print(authors, projects)
-log('done.')
-
 log('making projects directory...')
 if not os.path.exists(projects_path):
   os.mkdir(projects_path)
-log('done.')
-
-
-
-
-# AUTHOR PAGE
-# AUTHOR PAGE
-# AUTHOR PAGE
-log('building ' + authors_yml_path +'...')
-fp = open(authors_yml_path, 'w')
-for author in authors:
-  log(author)
-  fp.write('- author: ' + author + '\n\n')
-   # get all projects that this author is associated with
-  fp.write('  projects:\n\n')
-  for project in projects:
-    if author in projects[project]:
-      fp.write('    - name: ' + project + '\n')
-      fp.write('      url: ' + project + '\n\n')
-fp.close()
-log('done.')
-
-
-
-
-# PROJECT PAGES
-# PROJECT PAGES
-# PROJECT PAGES
-log('building project pages...')
-for entry in community['entries']:
-  project_name = entry['project_name']
-  sanitized_project_name = sanitize(project_name)
-  log('project: ' + project_name)
-  # write the project front matter
-  fp = open(projects_path + '/' + sanitized_project_name + '.md', 'w')
-  fp.write('---\n')
-  # layout, title, and permalink are needed by jekyll
-  fp.write('layout: project\n')
-  fp.write('title: ' + sanitized_project_name + '\n')
-  fp.write('permalink: /projects/' + sanitized_project_name + '\n')
-  # sanitized project name, raw project name is also available
-  fp.write('sanitized_project_name: ' + sanitized_project_name + '\n')
-  # authors list
-  fp.write('authors:\n')
-  for author in projects[sanitized_project_name]:
-    fp.write(' - ' + author + '\n')
-  # redirects list
-  fp.write('redirect_from:\n')
-  for author in projects[sanitized_project_name]:
-    fp.write(' - /authors/' + author + '/' + sanitized_project_name + '\n')
-  # raw data from community json
-  if 'author' in entry:
-    fp.write('raw_community_json_author: ' + entry['author'] + '\n')
-  if 'project_name' in entry:
-    fp.write('raw_community_project_name: ' + entry['project_name'] + '\n')
-  if 'project_url' in entry:
-    fp.write('raw_community_project_url: ' + entry['project_url'] + '\n')
-  if 'description' in entry:
-    fp.write('raw_community_description: ' + entry['description'] + '\n')
-  if 'discussion_url' in entry:
-    fp.write('raw_community_discussion_url: ' + entry['discussion_url'] + '\n')
-  if 'documentation_url' in entry:
-    fp.write('raw_community_documentation_url: ' + entry['documentation_url'] + '\n')
-  if 'tags' in entry:
-    fp.write('raw_community_tags:\n')
-    for tag in entry['tags']:
-      fp.write(' - ' + tag + '\n')
-  fp.write('---\n')
-  fp.close()
 log('done.')
 
 
@@ -195,15 +160,95 @@ fp = open('index.md', 'w')
 fp.write('---\n')
 fp.write('layout: index\n')
 fp.write('---\n')
-for entry in community['entries']:
-  project_name = entry['project_name']
-  sanitized_project_name = sanitize(project_name)
-  # [name](local doc) - [author](author filter page) - description
-  fragment_1 = '[' + project_name + '](/projects/' + sanitized_project_name + ')'
+for project in community_data.get_projects_in_alphabetical_order():
+  fragment_1 = '[' + project.raw_name + '](' + project.permalink + ')'
   fragment_2 = ''
-  for author in projects[sanitized_project_name]:
-    fragment_2 = fragment_2 + '[' + author + '](/author#' + author + ') '
-  fragment_3 = entry['description']
+  for author in project.get_authors_in_alphabetical_order():
+    fragment_2 = fragment_2 + '[' + community_data.authors[author].raw_name + '](/author#' + community_data.authors[author].sanitized_name + ') '
+  fragment_3 = project.description
   fp.write('- ' + fragment_1 + ' - ' + fragment_2 + ' - ' + fragment_3 + '\n')
 fp.close()
 log('done.')
+
+
+
+
+# AUTHOR PAGE
+# AUTHOR PAGE
+# AUTHOR PAGE
+log('building ' + authors_yml_path +'...')
+fp = open(authors_yml_path, 'w')
+for author in community_data.get_authors_in_alphabetical_order():
+  fp.write('- raw_name: ' + author.raw_name + '\n')
+  fp.write('  sanitized_name: ' + author.sanitized_name + '\n')
+  fp.write('  projects:\n\n')
+  for project in community_data.get_projects_in_alphabetical_order():
+    if author.raw_name in project.authors:
+      fp.write('    - raw_name: ' + project.raw_name + '\n')
+      fp.write('      url: ' + project.permalink + '\n\n')
+fp.close()
+log('done.')
+
+
+
+
+# PROJECT PAGES
+# PROJECT PAGES
+# PROJECT PAGES
+log('building project pages...')
+for project in community_data.get_projects_in_alphabetical_order():
+  # write the project front matter
+  fp = open(projects_path + '/' + project.sanitized_name + '.md', 'w')
+  fp.write('---\n')
+  # layout, title, and permalink are needed by jekyll
+  fp.write('layout: project\n')
+  fp.write('title: ' + project.raw_name + '\n')
+  fp.write('sanitized_name: ' + project.sanitized_name + '\n')
+  fp.write('permalink: ' + project.permalink + '\n')
+  fp.write('project_url: ' + project.project_url + '\n')
+  fp.write('description: ' + project.description + '\n')
+  fp.write('discussion_url: ' + project.discussion_url + '\n')
+  fp.write('documentation_url: ' + project.documentation_url + '\n')
+  fp.write('tags:\n')
+  for tag in project.tags:
+    fp.write(' - ' + tag + '\n')  
+  fp.write('authors:\n')
+  for author in project.authors:
+    fp.write(' - ' + author + '\n')
+  # recirects to mitigate against link rot from wiki.js (norns.community v1.0)
+  fp.write('redirect_from:\n')
+  for author in project.authors:
+    fp.write(' - /authors/' + author + '/' + project.sanitized_name + '\n')
+  fp.write('---\n')
+  fp.close()
+log('done.')
+
+
+
+
+
+
+# dev zone
+# project
+# ygg = community_data.projects['yggdrasil']
+# log(ygg.raw_name)
+# log(ygg.sanitized_name)
+# log(ygg.permalink)
+# log(ygg.description)
+# log(ygg.project_url)
+# log(ygg.discussion_url)
+# log(ygg.documentation_url)
+# print(sorted(ygg.tags))
+# print(sorted(ygg.authors))
+
+# author
+# ty = community_data.authors['tyleretters']
+# log(ty.raw_name)
+# log(ty.sanitized_name)
+# log(ty.permalink)
+# print(sorted(ty.projects))
+
+# for a in community_data.get_authors_in_alphabetical_order():
+#   log(a.raw_name)
+#   log(a.sanitized_name)
+#   log(a.permalink)
